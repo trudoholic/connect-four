b78cd47b5692020978626299a274c1878b17eec3
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-01-23 12:35:26 +0300

    init

---
+++ src/components/Board/types.ts
@@ -0,0 +1,7 @@
+import { ChipsPositions } from "../App/types";
+export interface Props {
+    columns: number;
+    rows: number;
+    chipsPositions: ChipsPositions;
+    onTileClick: (id: string) => any;
+}
---
+++ src/components/Column/types.ts
@@ -0,0 +1,7 @@
+import { ChipsPositions } from "../App/types";
+export interface Props {
+    column: number;
+    rows: number;
+    chipsPositions: ChipsPositions;
+    onTileClick: (id: string) => any;
+}
